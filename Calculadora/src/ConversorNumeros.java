7b980e0eaf9cbd9eaa0ad59e03ff74aaf92ddeb4
Robert Inácio <rig171210@gmail.com>
2026-09-22 23:30:39 -0300

    Add files via upload

---
+++ Calculadora/src/ConversorNumeros.java
@@ -0,0 +1,10 @@
+public class ConversorNumeros {
+    public Integer StringToInt(String num) {
+        int conversor = Integer.parseInt(num);
+    return (conversor);
+    }
+    public Double stringToDouble(String num){
+        double conversor = Double.parseDouble(num);
+        return (conversor);
+    }
+}
---
+++ Calculadora/src/Principal.java
@@ -0,0 +1,7 @@
+public class Principal {
+    static void main(String[] args) {
+        MenuCalculadora menu = new MenuCalculadora();
+        menu.executarCalculadora();
+    }
+
+}
